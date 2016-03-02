b3a28ef69857afe44be372858b7803a3ba84b0b5
upjd <qszhang19892008@163.com>
2016-03-02 21:17:51 +0800

    20160302产成品入库

---
+++ JWMSH/JWMSH/RptProStoreDetail.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace JWMSH
+{
+    public partial class RptProStoreDetail : Form
+    {
+        public RptProStoreDetail()
+        {
+            InitializeComponent();
+        }
+
+        private void RptProStoreDetail_Load(object sender, EventArgs e)
+        {
+            pageChange.Constr = BaseStructure.WmsCon;
+            pageChange.GetRecord();
+            //初始化表格功能控件
+            tsgfMain.FormId = Name.GetHashCode().ToString(CultureInfo.CurrentCulture);
+            tsgfMain.FormName = Text;
+            tsgfMain.Constr = BaseStructure.WmsCon;
+            tsgfMain.GetGridStyle(tsgfMain.FormId);
+        }
+
+        private void biSearch_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            var bfun = new WmsFunction(BaseStructure.WmsCon);
+            var strTemp = bfun.GetWhereSqlStr("ProStoreDetail");
+            if (string.IsNullOrEmpty(strTemp))
+                return;
+            pageChange.WhereStr = strTemp;
+            pageChange.GetRecord();
+            MessageBox.Show(@"查询成功", @"成功");
+        }
+
+        private void bbiRefresh_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            pageChange.GetRecord();
+        }
+
+        private void biExport_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            tsgfMain.SaveExcel2003();
+        }
+
+        private void biExit_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            Close();
+        }
+    }
+}
